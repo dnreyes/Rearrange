a3cf5df68ce31f4c5361a2885729b8bf2a98d28d
arloistale <jonathanlu321@gmail.com>
2015-04-28 17:52:13 -0700

    Initial Commit
    Here we go...

---
+++ BalloonGameSample/Assets/Scripts/BackgroundBlur.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundBlur : MonoBehaviour
+{
+	void Start ()
+	{
+		//SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		//Debug.Log (renderer.material
+	}
+}
---
+++ BalloonGameSample/Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainCharacter : MonoBehaviour
+{
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+	
+	}
+}
