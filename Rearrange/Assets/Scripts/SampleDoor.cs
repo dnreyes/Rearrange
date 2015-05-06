d75da51bc5aa34ce881d075dbab946912645ddff
PriscillaUsmani <pusmani@ucsc.edu>
2015-05-06 16:18:46 -0700

    Button- Getting Closer

--- Rearrange/Assets/Scripts/SampleDoor.cs
+++ Rearrange/Assets/Scripts/SampleDoor.cs
@@ -6,15 +6,44 @@ public class SampleDoor : MonoBehaviour {
 
 	public Text displayText;
 //
-	void onMouseOver() {
-		if (Input.GetMouseButtonDown (0)) {
-			print( "yellowwwwwwwwwwwwwwwwwwwwwwwwwwwww");
-		}
+//	void onMouseOver() {
+//		if (Input.GetMouseButtonDown (0)) {
+//			var hit : RaycastHit;
+//			var ray : Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//			if (Physics.Raycast (ray, hit, 100.0))
+//			{
+//				SampleDoor = hit.collider.gameObject;
+//				if(SampleDoor.Sample=="spritesheet_0")
+//				{ 
+//		print( "yellowwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+//			}
+//		}
+//
+//		}
+//	}
+//
+//	void Update(){
+//		onMouseOver ();
+//
+//	}
 
+
+
+	void OnMouseDown(){
+		print("YELLOW");
 	}
-	void Update(){
-		onMouseOver ();
 
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			if (Physics.Raycast(ray, out hit)) {
+				print("Yellow");
+			} else {
+				print("Hit nothing");
+			}
+		}
 	}
 //	function OnMouseDown(){
 //			Debug.log ("YEELLLOW OMG PRINT YELLOW");
