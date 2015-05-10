bab58fc27ccfbd8c5394f2db64a7853abe316e40
Delmy Reyes <dnreyes@ucsc.edu>
2015-05-10 14:12:27 -0700

    Click Works
    Hopefully.

--- Rearrange/Assets/Scripts/WordManager.cs
+++ Rearrange/Assets/Scripts/WordManager.cs
@@ -6,13 +6,26 @@ using System.IO;
 
 public class WordManager : MonoBehaviour {
 
-    void CreateXML()
-    {
+  /*  public TextAsset WordAsset;
+    public static List<Item> words = new List<Item>{};
+    public List<Item> currWord = new List<Item>();
 
-    }
-    void LoadXML()
+    private List<Dictionary<string, string>> wordDict = new List<Dictionary<string, string>>;
+    private Dictionary<string, string> obj;
+
+    string _FileLoc, _Filename;
+
+    void Start()
     {
-        
+        _FileLoc = Application.datapath + "/Resources";
+        _Filename = "WordBank.xml";
+    }
+
+    void ReadXML()
+    {
+        XmlDocument solution = new XmlDocument();
+        solution.LoadXml(WordAsset.text);
+        XmlNodeList wordList = solution.GetElementsByTagName("Solutions1");
     }
 
     //Anything we want to store in a XML file
@@ -21,5 +34,5 @@ public class WordManager : MonoBehaviour {
         public string _name;
 
         public SolutionsUsed() { }
-    }
+    }*/
 }
