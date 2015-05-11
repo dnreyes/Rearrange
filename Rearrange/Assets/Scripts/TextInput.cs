21f97f2d9ec911337f51da8a434f6304bf3a1a75
asksuyo <alittlemerm8@yahoo.com>
2015-05-10 21:53:43 -0700

    More art and checks in place
    added script level1.cs that is connected to the camera
    level1.cs checks valid statements

--- Rearrange/Assets/Scripts/TextInput.cs
+++ Rearrange/Assets/Scripts/TextInput.cs
@@ -8,13 +8,10 @@ public class TextInput: MonoBehaviour {
 	private string sInput;
 	public Text tInput;
 	public InputField iInput;
-	public Animation anim;
-
 
 	// Update is called once per frame
 	void Update () {
-		//anim = GetComponent<Animation>();
-		//anim.Stop ();
+
 		if (Input.GetKeyDown ("return")) {
 			//set the text field to be user input
 			tInput.text = iInput.text;
