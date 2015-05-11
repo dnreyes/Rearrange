8f449f052d0719263b958aa0157a0f7f119040c6
Delmy Reyes <dnreyes@ucsc.edu>
2015-05-10 17:00:05 -0700

    Saving for the day
    Changes:
    -Click works on Main.unity
    -WordBank.cs is filled and seems to be reading in xml. ***Not Done
    Completely.***
    -TextInput doesn't yell at you about null object reference.

--- Rearrange/Assets/Scripts/TextInput.cs
+++ Rearrange/Assets/Scripts/TextInput.cs
@@ -5,26 +5,28 @@ using UnityEditor;
 
 public class TextInput: MonoBehaviour {
 
-	//private string sInput;
+	private string sInput;
 	public Text tInput;
 	public InputField iInput;
 
 	//get input from text field
 	//print input into textbox after pressing enter
-
+    void Start()
+    {
+        sInput = "";
+    }
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("return")) {
 			//set the text field to be user input
 			tInput.text = iInput.text;
 			//save user input in a string
-		//	sInput = iInput.text;
+		    sInput = iInput.text;
 		}
 
-        //Error???
-		//else if (sInput.Equals("hole")) {
-			//set the text field to win state
-			//tInput.text = "yay! you won!";
-		//}
+        if (sInput.Equals("hole"))
+        {
+            tInput.text = "Yay! You Won!";
+        }
 	}
 }
